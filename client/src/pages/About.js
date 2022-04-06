09cf2358e70755719b50ffa0a973451ccfa940f5
Debbie Hall <debleehall15@gmail.com>
2022-04-06 19:41:29 -0400

    about page set up

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        <h1>Who We Are.</h1>
+        <p>Bootcamp Zoo is a place where everyone can come together and share there love for animals and help build habitats for them.</p>
+        <p>Come explore our animals and find your place in our projects.</p>
+      </div>
+    </main>
+  );
+};
+
+export default About;
