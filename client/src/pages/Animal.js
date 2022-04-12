859a85d51449102ae815dda1fe0e739af262f861
cmrotruck <crotruck@gmail.com>
2022-04-11 20:36:14 -0400

    heroku test

--- client/src/pages/Animal.js
+++ client/src/pages/Animal.js
@@ -16,14 +16,14 @@ const Animals = () => {
           deployed="https://cmrotruck.github.io/recipe-search/"
         />
 
-        {/* <AnimalCard
+        <AnimalCard
           title="Recipe Search"
           created="2021"
           description="Population status is valnerable. Cheetahs are the most endangered cat in Africa, only an estimated 7,000-10,000 remain. Molecular genetic studies on free-ranging and captive cheetahs have shown that the species lacks genetic variation, probably due to past inbreeding. The consequences of such genetic uniformity have led to reproductive abnormalities, high infant mortality, and greater susceptibility to disease, causing the species to be less adaptable and more vulnerable to ecological and environmental changes."
           screenshot={Cheetah}
           github="https://github.com/cmrotruck/recipe-search"
           deployed="https://cmrotruck.github.io/recipe-search/"
-        /> */}
+        />
       </Card.Group>
     </Dimmer.Dimmable>
   );
