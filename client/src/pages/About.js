7454dea734a6691a0b402d70797edc641b0f3020
Debbie Hall <debleehall15@gmail.com>
2022-04-12 23:58:35 -0400

    tested and fixed colors for web accessibility

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Logo from "../images/logo/zoologo.jpg";
+import { Image } from "semantic-ui-react";
 
 const About = () => {
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        <h1>Who We Are.</h1>
-        <p>Bootcamp Zoo is a place where everyone can come together and share there love for animals and help build habitats for them.</p>
-        <p>Come explore our animals and find your place in our projects.</p>
-      </div>
+      <Image className="animalImage" src={Logo} alt="bootcamp zoo logo" rounded />
+      <h1 className="aboutHeading">Who We Are.</h1>
+      <p className="about">Bootcamp Zoo is a place where everyone can come together and share there love for animals and help build habitats for them.</p>
+      <p className="about">Come explore our animals and find your place in our projects.</p>
     </main>
   );
 };
