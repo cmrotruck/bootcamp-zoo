9d3429457785c020f17c4bb549a844ebc20b17bc
Debbie Hall <debleehall15@gmail.com>
2022-04-13 11:27:28 -0400

    changed there to their on about page, fixed cheeta spelling so picture would appear, got rig of commented out code no longer needed

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <main>
       <Image className="animalImage" src={Logo} alt="bootcamp zoo logo" rounded />
       <h1 className="aboutHeading">Who We Are.</h1>
-      <p className="about">Bootcamp Zoo is a place where everyone can come together and share there love for animals and help build habitats for them.</p>
+      <p className="about">Bootcamp Zoo is a place where everyone can come together and share their love for animals and help build habitats for them.</p>
       <p className="about">Come explore our animals and find your place in our projects.</p>
     </main>
   );
--- client/src/pages/Animal.js
+++ client/src/pages/Animal.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AnimalCard from "../components/Animals/AnimalCard";
 import { Card, Dimmer, Segment } from "semantic-ui-react";
-// import Cheetah from "../images/animals/cheetah.JPG";
 import { useQuery } from "@apollo/client";
 import { QUERY_ANIMALS } from "../utils/queries";
 
@@ -30,15 +29,6 @@ const Animals = () => {
             );
           })
         )}
-
-        {/* <AnimalCard
-          title="Recipe Search"
-          created="2021"
-          description="Population status is valnerable. Cheetahs are the most endangered cat in Africa, only an estimated 7,000-10,000 remain. Molecular genetic studies on free-ranging and captive cheetahs have shown that the species lacks genetic variation, probably due to past inbreeding. The consequences of such genetic uniformity have led to reproductive abnormalities, high infant mortality, and greater susceptibility to disease, causing the species to be less adaptable and more vulnerable to ecological and environmental changes."
-          screenshot={Cheetah}
-          github="https://github.com/cmrotruck/recipe-search"
-          deployed="https://cmrotruck.github.io/recipe-search/"
-        /> */}
       </Card.Group>
     </Dimmer.Dimmable>
   );
